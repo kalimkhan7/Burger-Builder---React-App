f37eec4f6cf513357ae025258b3edf30a8b84e1e
kalimkhan7 <kalimullah.khan@raweng.com>
2019-06-13 18:48:25 +0530

    Controls Added

---
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'
+import Burger from '../../Components/Burger/Burger'
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls'
+
+class BurgerBuilder extends React.Component{
+
+    state = {
+        ingredients: {
+            meat:0,
+            cheese: 0,
+            bacon: 0,
+            salad: 0
+
+        }
+        
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls/>
+            </Aux>
+        );
+    }
+} 
+
+export default BurgerBuilder;
